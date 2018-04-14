03c883b6fff69e3edcb283f1064bf187af6f33e3
Gaurav Garg <gauravsngarg@gmail.com>
2018-04-14 08:07:59 +0400

    Add Recycler View

--- app/src/main/java/gauravsngarg/com/popularmovies/MainActivity.java
+++ app/src/main/java/gauravsngarg/com/popularmovies/MainActivity.java
@@ -2,12 +2,24 @@ package gauravsngarg.com.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int NUMBER_OF_LISTITEMS = 20;
+
+    private RecyclerView mMoviesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMoviesList = (RecyclerView) findViewById(R.id.rv_movies);
+
+
+        mMoviesList.setHasFixedSize(true);
     }
+
+
 }
