4e92cd536c8ebb863073e3a6bd461f32c19e5bf2
Gaurav Garg <gauravsngarg@gmail.com>
2018-04-15 01:14:39 +0400

    Add URLBuild for Movie Thumbnail

--- app/src/main/java/gauravsngarg/com/popularmovies/adapter/MovieAdapter.java
+++ app/src/main/java/gauravsngarg/com/popularmovies/adapter/MovieAdapter.java
@@ -5,12 +5,17 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
+import java.net.URL;
 import java.util.List;
 
 import gauravsngarg.com.popularmovies.R;
 import gauravsngarg.com.popularmovies.model.MovieItem;
+import gauravsngarg.com.popularmovies.utils.GenerateMovieThumbnailsURL;
 
 /**
  * Created by GG on 14/04/18.
@@ -26,20 +31,22 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MoviesViewHo
     public MovieAdapter(int movieCount, Context mContext, List<MovieItem> listitem) {
         mMovieItemsCount = movieCount;
         list = listitem;
-        count =0;
+        count = 0;
     }
 
     @Override
     public MoviesViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         Context mContext = parent.getContext();
-
         LayoutInflater inflator = LayoutInflater.from(mContext);
 
         View view = inflator.inflate(R.layout.list_item, parent, false);
-
         MoviesViewHolder viewHolder = new MoviesViewHolder(view);
-
         viewHolder.mMovieDetail.setText(list.get(count).getMovieTitle());
+        viewHolder.mMovieDetail.setVisibility(View.GONE);
+
+        URL url = null;
+        url = GenerateMovieThumbnailsURL.buildURL(list.get(count).getMoviePosterPath());
+        Picasso.with(mContext).load(url.toString()).into(viewHolder.mMovieItem);
 
         count++;
         return viewHolder;
@@ -47,9 +54,6 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MoviesViewHo
 
     @Override
     public void onBindViewHolder(MoviesViewHolder holder, int position) {
-
-       // holder.bind(position);
-
     }
 
     @Override
@@ -57,15 +61,16 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MoviesViewHo
         return mMovieItemsCount;
     }
 
-    class MoviesViewHolder extends RecyclerView.ViewHolder{
+    class MoviesViewHolder extends RecyclerView.ViewHolder {
 
         TextView mMovieDetail;
-
+        ImageView mMovieItem;
 
         public MoviesViewHolder(View itemView) {
             super(itemView);
 
             mMovieDetail = (TextView) itemView.findViewById(R.id.tv_item);
+            mMovieItem = (ImageView) itemView.findViewById(R.id.iv_movieitem);
 
         }
     }
